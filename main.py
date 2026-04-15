de51774a562e8dffd1a0e3c8e4306ef62556f5ca
Nehemiah Daniel <danielnehemiah15@gmail.com>
2026-04-15 10:25:00 +0100

    Add CORS

--- main.py
+++ main.py
@@ -1,7 +1,16 @@
 from fastapi import FastAPI, status
 from fastapi.responses import JSONResponse
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["GET"],
+    allow_headers=["*"],
+)
 
 
 @app.get("/")
